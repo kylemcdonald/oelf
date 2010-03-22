4522516cc951deebd8412478d3d0ea0ff698a971
kylecimcdonald <kylecimcdonald@a1385046-1765-11df-945e-a3343a1c6be9>
2010-03-22 05:39:46 +0000

    small values for table b are output

--- trunk/cpp/mp3glib/src/main.cpp
+++ trunk/cpp/mp3glib/src/main.cpp
@@ -1,19 +1,17 @@
 #include "mp3glib.h"
 
 int main() {
+	Granule::buildLookup();
+
 	ofstream file;
 	file.open("out.mp3", std::ios::binary | std::ios::out);
 	long double samples = 0;
-	long double samplerate = 44100;
 	long double bits = 0;
-	long double target = 128000;
-	while(bits / 8 < 4369345) {
+	while(bits / 8 < 60 * 16000) {
 		Frame frame;
 		if(samples != 0) {
-			//cout << "(bits * samplerate): " << (bits * samplerate) << endl;
-			long double cur = (bits * samplerate) / samples;
-			//cout << cur << "bps" << endl;
-			if(cur < target)
+			long double curBitrate = (bits * frame.header.getSamplerate()) / samples;
+			if(curBitrate < frame.header.getBitrate())
 				frame.header.setPadding(true);
 			else
 				frame.header.setPadding(false);
@@ -21,7 +19,6 @@ int main() {
 		frame.write(file);
 		samples += FRAME_SAMPLES;
 		bits += frame.getSize() * 8;
-		//cout << "samples: " << samples << " bits: " << bits << endl;
 	}
 	file.close();
 }
