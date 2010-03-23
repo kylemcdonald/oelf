1fb69c8056eef2dfbd41a0ed5ba232b171749fe3
kylecimcdonald <kylecimcdonald@a1385046-1765-11df-945e-a3343a1c6be9>
2010-03-23 01:03:41 +0000

    aha, those last three bars are the subblock gain for short windows. doesn't explain that last bar in the case of long blocks. about to implement basic big value exporting for small big values.

--- cpp/mp3glib/src/main.cpp
+++ cpp/mp3glib/src/main.cpp
@@ -9,6 +9,7 @@ int main() {
 	long double bits = 0;
 	while(bits / 8 < 1 * 16000) {
 		Frame frame;
+
 		if(samples != 0) {
 			long double curBitrate = (bits * frame.header.getSamplerate()) / samples;
 			if(curBitrate < frame.header.getBitrate())
@@ -18,15 +19,19 @@ int main() {
 		}
 
 		Granule& gr = frame.granules[0];
+		gr.scaleShift = true;
 		gr.blockType = SHORT_BLOCK;
-		gr.mixedBlock = false;
-		gr.slindex = 14;
-		for(int i = 0; i < BANDS; i++)
-			gr.sfi[i] = i;
-		for(int i = 0; i < SUBBLOCKS; i++)
-			for(int j = 0; j < SHORT_BANDS; j++)
-				gr.sfiShort[i][j] = i;
-
+		gr.slindex = 15;
+		for(int j = 0; j < SHORT_BANDS; j++) {
+			for(int i = 0; i < SUBBLOCKS; i++)
+				gr.sfiShort[i][j] = 0xff;
+			gr.subblockGain[j] = 0xff;
+		}
+/*
+		gr.bigValues = 5;
+		for(int i = 0; i < gr.bigValues * 2; i++)
+			gr.bigCodes[i] = (i % 3) - 1;
+*/
 		frame.write(file);
 		samples += FRAME_SAMPLES;
 		bits += frame.getSize() * 8;
