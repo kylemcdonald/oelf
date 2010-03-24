11ecb61ab6d957fec5da7f2a7407ccbbbd006bb4
kylecimcdonald <kylecimcdonald@a1385046-1765-11df-945e-a3343a1c6be9>
2010-03-24 03:29:25 +0000

    successfully outputting small values! the issue with short blocks still isn't resolved, but i'm going to avoid them for now and move on. i've stopped thinking about the basic type as the frame, but instead the granule (13 ms). time to start arranging and enumerating.

--- trunk/cpp/mp3glib/src/main.cpp
+++ trunk/cpp/mp3glib/src/main.cpp
@@ -36,19 +36,23 @@ int main() {
 		for(int i = 0; i < BANDS; i++)
 			gr.sfi[i] = 4;
 
-		for(int i = 0; i < REGIONS; i++) {
-			gr.regionCount[i] = 6;
+		for(int i = 0; i < REGIONS; i++)
 			gr.bigTableSelect[i] = 15;
-		}
 
-		gr.regionCount[0] = 9;
-		gr.regionCount[1] = 5;
-		gr.regionCount[2] = 6;
+		gr.regionCount[0] = 8;
+		gr.regionCount[1] = 4;
+		gr.regionCount[2] = 2;
 
 		int bv = gr.getBigValues();
 		for(int i = 0; i < bv; i++) {
-			gr.bigCodes[i] = 1 + (5 * i) / bv;
+			gr.bigCodes[i] = 1 + (14 * i) / bv;
 		}
+
+		int smallValues = 576 - bv;
+		smallValues /= 4;
+		gr.smallValues = smallValues;
+		for(int i = 0; i < smallValues; i++)
+			gr.smallCodes[i] = 41;
 
 		frame.write(file);
 		samples += FRAME_SAMPLES;
