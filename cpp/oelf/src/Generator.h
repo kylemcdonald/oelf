c4105c531949745c19b11e50c1b3bd351f40e783
kylecimcdonald <kylecimcdonald@a1385046-1765-11df-945e-a3343a1c6be9>
2010-03-28 09:34:43 +0000

    i've got most things in a place i like. but the beginning betrays the middle. it's fine, i just don't encourage anyone to listen past the end of the universe. it starts to get a bit scary.

--- cpp/oelf/src/Generator.h
+++ cpp/oelf/src/Generator.h
@@ -5,13 +5,23 @@
 class Generator {
 public:
 	Generator();
+	~Generator();
+	void reset();
+	void reflect();
 	void setOrder(int order);
+	int getOrder();
 	void desync();
 	void makeNext();
 	void write(ostream& out);
 	int getMagnitude();
+	int getSize();
+	const BigInteger& getFrame();
+	const BigInteger& getGray();
 private:
-	BigInteger frameBuffer, mask;
+	int size;
+	BigInteger frameBuffer;
+
+	int* swap;
 
 	Frame frame;
 	long double samples;
