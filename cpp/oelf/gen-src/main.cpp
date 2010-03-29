9c4af119158267258b96e7c6dbf61d573c557ef7
kylecimcdonald <kylecimcdonald@a1385046-1765-11df-945e-a3343a1c6be9>
2010-03-29 00:25:53 +0000

    that's it. it seems to be running fine and changing reading/writing state information correctly. now for a couple days of pr, preparing for tuesday's show, and finishing the written thesis...

--- cpp/oelf/gen-src/main.cpp
+++ cpp/oelf/gen-src/main.cpp
@@ -8,28 +8,47 @@
 using namespace std;
 
 Generator generator;
-bool shouldReset;
 
-void generateMp3() {
-	system("move *.mp3 backup");
-
+string generateMp3() {
 	ostringstream filename;
-	filename << time(NULL) << "-" << generator.getOrder() << "-" << SKIP_AMOUNT << ".mp3";
+	filename << time(NULL) << ".mp3";
 
 	ofstream file;
-	file.open(filename.str().c_str(), std::ios::binary | std::ios::out);
-	if(shouldReset)
-		generator.reset();
-	for(int i = 0; i < (60 * 60 * 44100) / 1152; i++) {
-	//for(int i = 0; i < 512; i++) {
+	file.open(filename.str().c_str(), ios::binary | ios::out);
+	int max = (60 * 60 * 44100) / 1152;
+	for(int i = 0; i < max; i++) {
 		generator.makeNext();
 		generator.write(file);
+		if(i % (max / 10) == 0)
+			cout << i << " / " << max << " frames" << endl;
 	}
 	file.close();
 	cout << "Wrote " << filename.str() << " out." << endl;
+
+	return filename.str();
 }
 
 int main() {
-	cout << "hello" << endl;
+	ifstream readState;
+	readState.open("state.txt", ios::in);
+	generator.readState(readState);
+	readState.close();
+
+	cout << "Loaded state from file." << endl;
+
+	string filename = generateMp3();
+
+	ofstream writeState;
+	writeState.open("state.txt", ios::out);
+	generator.writeState(writeState);
+	writeState.close();
+
+	cout << "Wrote out modified state." << endl;
+
+	ostringstream command;
+	command << "\"C:\\Program Files\\Winamp\\winamp.exe\" /ADD ";
+	command << filename;
+	system(command.str().c_str());
+
 	return 0;
 }
